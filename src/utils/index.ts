5df89602d45515cd9de48607069a216578001215
saad-nkurunziza <saddynkurunziza8@gmail.com>
2023-08-04 00:31:42 +0200

    provider

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,9 @@
 import prisma from "@/libs/prisma";
+import { getServerSession, NextAuthOptions, User } from "next-auth";
 import axios from "axios";
+import { AdapterUser } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+
 export const getTweets = async () => {
   const response = await axios.get("http://localhost:3000/api/tweets");
   const tweets = await response.data;
@@ -32,3 +36,39 @@ export const handleLike = async (id: string) => {
   console.log(tweet);
   return tweet;
 };
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_ID!,
+    }),
+  ],
+  theme: {
+    colorScheme: "dark",
+  },
+  // session: {
+  //   strategy: "jwt",
+  //   maxAge: 30 * 24 * 60 * 60,
+  //   updateAge: 24 * 60 * 60,
+  // },
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
+
+export async function getCurrentUser() {
+  const session = await getServerSession(authOptions);
+
+  return session;
+}
